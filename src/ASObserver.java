98f75b19057c5b3bc874628c849079e075609f45
DarthVi <covinc93@gmail.com>
2018-06-03 00:12:20 +0200

    adding an observer (not yet completed)

---
+++ src/ASObserver.java
@@ -0,0 +1,16 @@
+import peersim.core.Control;
+
+public class ASObserver implements Control
+{
+    @Override
+    public boolean execute()
+    {
+        //TODO: do stuff here
+        return false;
+    }
+
+    private void serializeNetworkSnapshot()
+    {
+        //TODO: do stuff here
+    }
+}
