e1d60ec7c7a3c03cfb8f413140e8be4720b4d4a8
DarthVi <covinc93@gmail.com>
2018-06-04 17:40:01 +0200

    One unique edge file created, differente serialized nodes.csv

--- src/ASObserver.java
+++ src/ASObserver.java
@@ -18,6 +18,7 @@ public class ASObserver implements Control
     //CONSTANTS
     private static final String PAR_ITERINTERVAL = "logInterval";
     private static final String PAR_PROT = "protocol";
+    private static final String F_VALUE = "f_value";
 
     private static final String NODEFILENAME = "nodes";
     private static final String EDGEFILENAME = "edges";
@@ -27,24 +28,53 @@ public class ASObserver implements Control
     private int iterations;
     private int interval;
     private int pid;
+    private int culturalCodeSize;
+
+    private CSVPrinter edgePrinter;
 
     public ASObserver(String prefix)
     {
         interval = Configuration.getInt(prefix + "." + PAR_ITERINTERVAL);
         pid = Configuration.getPid(prefix + "." + PAR_PROT);
         iterations = 0;
+        culturalCodeSize = Configuration.getInt(prefix + "." + F_VALUE);
+
+        FileWriter outEdges = null;
+
+        try
+        {
+            outEdges = new FileWriter("./snapshots/" + EDGEFILENAME  + ".csv");
+
+            edgePrinter = new CSVPrinter(outEdges, CSVFormat.DEFAULT.withHeader(EDGE_HEADERS));
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Override
     public boolean execute()
     {
-        //serialize a snapshot of the network current state
+        //serialize the edges only one time (they do not change in this model), then serialize network snapshot
+        if(iterations == 0)
+        {
+            serializeEdges();
+        }
+
         iterations++;
         if(iterations % interval == 0)
             serializeNetworkSnapshot(iterations);
 
         //if no imitation or movements occur, stop the simulation
-        return (Interaction.getCulturalChanges() == false && Interaction.getMoveActivity() == false && iterations != 1);
+        if((Interaction.getCulturalChanges() == false && Interaction.getMoveActivity() == false && iterations != 1) == true)
+        {
+            closeCSVPrinters();
+            return true;
+        }
+        else
+            return false;
     }
 
     /**
@@ -56,33 +86,17 @@ public class ASObserver implements Control
      */
     private void serializeNetworkSnapshot(int iterAppend)
     {
-        String[] NODE_HEADERS = null;
+
         FileWriter outNodes = null;
-        FileWriter outEdges = null;
         CSVPrinter nodePrinter = null;
-        CSVPrinter edgePrinter = null;
 
-
-        for(int i=0; i < Network.size(); i++)
+        try
         {
-            Site site = (Site) Network.get(i);
-            Linkable linkable = (Linkable) site.getProtocol(FastConfig.getLinkable(pid));
-
-            //collect site data
-            long siteID = site.getID();
-            boolean isEmpty = site.isEmpty();
-            int[] culturalCode = site.getWholeSigma();
-
-            //collecte neighbors IDs
-            List<Long> neighborsID = new ArrayList<Long>();
-
-            for(int j = 0; j < linkable.degree(); j++)
-            {
-                neighborsID.add(linkable.getNeighbor(j).getID());
-            }
+            outNodes = new FileWriter("./snapshots/" + NODEFILENAME + iterAppend  + ".csv");
+            String[] NODE_HEADERS;
 
             //node headers depends on cultural code size, cannot be defined as a static final field
-            NODE_HEADERS = new String[3 + culturalCode.length];
+            NODE_HEADERS = new String[3 + culturalCodeSize];
             NODE_HEADERS[0] = "Id";
             NODE_HEADERS[1] = "Empty";
             NODE_HEADERS[2] = "Interval";
@@ -91,37 +105,27 @@ public class ASObserver implements Control
             {
                 NODE_HEADERS[j] = "code" + (j-3);
             }
+            nodePrinter = new CSVPrinter(outNodes, CSVFormat.DEFAULT.withHeader(NODE_HEADERS).withQuote(null));
 
-            if(nodePrinter == null || edgePrinter == null)
-            {
-                try
-                {
-                    outNodes = new FileWriter("./snapshots/" + NODEFILENAME + iterAppend + ".csv");
-                    outEdges = new FileWriter("./snapshots/" + EDGEFILENAME + iterAppend + ".csv");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
-                    nodePrinter = new CSVPrinter(outNodes, CSVFormat.DEFAULT.withHeader(NODE_HEADERS).withQuote(null));
-                    edgePrinter = new CSVPrinter(outEdges, CSVFormat.DEFAULT.withHeader(EDGE_HEADERS));
-                }
-                catch(IOException e)
-                {
-                    System.out.println(e.getMessage());
-                    e.printStackTrace();
-                }
-            }
+
+        for(int i=0; i < Network.size(); i++)
+        {
+            Site site = (Site) Network.get(i);
+
+            //collect site data
+            long siteID = site.getID();
+            boolean isEmpty = site.isEmpty();
+            int[] culturalCode = site.getWholeSigma();
+
 
             try
             {
-
-                nodePrinter.printRecord(siteID, isEmpty, "\"[" + iterAppend + ", " + (iterAppend+interval) + "]\"", Arrays.toString(culturalCode).replaceAll("[\\\" \\[\\]]", ""));
-
-
-                for(long id : neighborsID)
-                {
-                    edgePrinter.printRecord(siteID, id);
-                }
-
-
-
+                nodePrinter.printRecord(siteID, isEmpty, "\"[" + iterAppend + ", " + (iterAppend + 2) + "\"]", Arrays.toString(culturalCode).replaceAll("[\\\" \\[\\]]", ""));
             }
             catch(IOException | NullPointerException e)
             {
@@ -132,11 +136,55 @@ public class ASObserver implements Control
         try
         {
             nodePrinter.close();
+        }
+        catch(IOException | NullPointerException e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public void closeCSVPrinters()
+    {
+        try
+        {
             edgePrinter.close();
         } catch (IOException | NullPointerException e)
         {
             e.printStackTrace();
         }
+    }
 
+    public void serializeEdges()
+    {
+        for(int i=0; i < Network.size(); i++)
+        {
+            Site site = (Site) Network.get(i);
+            Linkable linkable = (Linkable) site.getProtocol(FastConfig.getLinkable(pid));
+
+            //collect site data
+            long siteID = site.getID();
+
+            //collecte neighbors IDs
+            List<Long> neighborsID = new ArrayList<Long>();
+
+            for(int j = 0; j < linkable.degree(); j++)
+            {
+                neighborsID.add(linkable.getNeighbor(j).getID());
+            }
+
+            for(long id : neighborsID)
+            {
+                try
+                {
+                    edgePrinter.printRecord(siteID, id);
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
